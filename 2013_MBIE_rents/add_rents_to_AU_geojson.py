eeebfd101b27065b1ba13badc6051af6a4deb225
Alex Raichev <alex@raichev.net>
2014-02-02 17:22:47 +1300

    Cleaned up directory structure and filenames

--- 2013_MBIE_rents/add_rents_to_AU_geojson.py
+++ 2013_MBIE_rents/add_rents_to_AU_geojson.py
@@ -8,13 +8,13 @@ import csv, json
 
 # Input files
 rent_csv = 'data/mean_rents.csv'
-geodata_geojson = 'data/Auckland_AUs_2013_small.geojson'
-aus_and_suburbs_csv = 'data/aus_and_suburbs.csv'
+geodata_geojson = 'data/Auckland_AUs_2013.geojson'
+aus_and_suburbs_csv = 'data/Auckland_AUs_2013_and_suburbs.csv'
 AUCKLAND_RENT_REGIONS = {'Auckland', 'Franklin', 'Manukau', 'North Shore', 
   'Papakura', 'Rodney', 'Waitakere'}
 
 # Output file
-unified_geojson = 'data/Auckland_AUs_2013_small_with_rents.geojson'
+unified_geojson = 'data/Auckland_AUs_2013_with_rents.geojson'
 
 # Convert rent_csv data into a nested dictionary keyed by rent suburb
 rent_by_nbedrooms_by_property_by_suburb = dict()
@@ -36,7 +36,7 @@ with open(rent_csv, 'rb') as csv_file:
           prop].keys():
             rent_by_nbedrooms_by_property_by_suburb[au][prop][nbedrooms] = rent
 
-"""
+
 # Print rent suburbs for reference
 suburbs = sorted(rent_by_nbedrooms_by_property_by_suburb.keys())
 print('rent suburbs ({!s}): '.format(len(suburbs)))
@@ -53,7 +53,7 @@ print('aus ({!s}): '.format(len(aus)))
 for au in aus:
     print(au)
 print()
-"""
+
 
 # Use aus_and_suburbs.csv to convert the dictionary keys 
 # from rent suburb to AU 
