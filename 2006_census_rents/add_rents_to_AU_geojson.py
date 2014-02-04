6073d77e9a056ee0c837730c448f6d71ec7b6366
Alex Raichev <alex@raichev.net>
2014-02-04 20:46:20 +1300

    Removed property type selector to get less spotty data. Played with map styling

--- 2006_census_rents/add_rents_to_AU_geojson.py
+++ 2006_census_rents/add_rents_to_AU_geojson.py
@@ -17,22 +17,28 @@ AUCKLAND_RENT_REGIONS = {'Auckland', 'Franklin', 'Manukau', 'North Shore',
 unified_geojson = 'data/Auckland_AUs_2013_with_rents.geojson'
 
 # Convert rent_csv data into a nested dictionary keyed by rent suburb
-rent_by_nbedrooms_by_suburb = dict()
+rent_by_nbedrooms_by_property_by_suburb = dict()
 with open(rent_csv, 'rb') as csv_file:
     reader = csv.reader(csv_file)
     reader.next() # Skip header row
     for row in reader:
-        region, suburb, prop, nbedrooms, rent = row[:4] + [row[-1]]
-        if region not in AUCKLAND_RENT_REGIONS or prop != 'Total':
+        region, au, prop, nbedrooms, rent = row[:4] + [row[-1]]
+        if region not in AUCKLAND_RENT_REGIONS or prop == 'Total':
             # Skip row
             continue
-        if suburb not in rent_by_nbedrooms_by_suburb.keys():
-            rent_by_nbedrooms_by_suburb[suburb] = {nbedrooms: rent}
-        elif nbedrooms not in rent_by_nbedrooms_by_suburb[suburb].keys():
-            rent_by_nbedrooms_by_suburb[suburb][nbedrooms] = rent
+        if au not in rent_by_nbedrooms_by_property_by_suburb.keys():
+            rent_by_nbedrooms_by_property_by_suburb[au] =\
+              {prop: {nbedrooms: rent}}
+        elif prop not in rent_by_nbedrooms_by_property_by_suburb[au].keys():
+            rent_by_nbedrooms_by_property_by_suburb[au][prop] =\
+              {nbedrooms: rent}
+        elif nbedrooms not in rent_by_nbedrooms_by_property_by_suburb[au][
+          prop].keys():
+            rent_by_nbedrooms_by_property_by_suburb[au][prop][nbedrooms] = rent
+
 
 # Print rent suburbs for reference
-suburbs = sorted(rent_by_nbedrooms_by_suburb.keys())
+suburbs = sorted(rent_by_nbedrooms_by_property_by_suburb.keys())
 print('rent suburbs ({!s}): '.format(len(suburbs)))
 for suburb in suburbs:
     print(suburb)
@@ -48,20 +54,21 @@ for au in aus:
     print(au)
 print()
 
+
 # Use aus_and_suburbs.csv to convert the dictionary keys 
 # from rent suburb to AU 
-rent_by_nbedrooms_by_au = dict()
+rent_by_nbedrooms_by_property_by_au = dict()
 with open(aus_and_suburbs_csv, 'rb') as csv_file:
     reader = csv.reader(csv_file)
     reader.next() # Skip header row
     for row in reader:
         au, suburb = row
         if suburb:
-            rent_by_nbedrooms_by_au[au] =\
-              rent_by_nbedrooms_by_suburb[suburb]
+            rent_by_nbedrooms_by_property_by_au[au] =\
+              rent_by_nbedrooms_by_property_by_suburb[suburb]
         else:
             # No data
-            rent_by_nbedrooms_by_au[au] = 'NA'
+            rent_by_nbedrooms_by_property_by_au[au] = 'NA'
 
 
 # Add rent data to geometry data from geodata_geojson 
@@ -71,8 +78,8 @@ with open(geodata_geojson, 'rb') as json_file:
 
 for feature in geodata['features']:
     props = feature['properties']
-    props['rent_by_nbedrooms'] =\
-      rent_by_nbedrooms_by_au[props['AU_NAME']]
+    props['rent_by_nbedrooms_by_property'] =\
+      rent_by_nbedrooms_by_property_by_au[props['AU_NAME']]
 
 with open(unified_geojson, 'w') as json_file:
     json.dump(geodata, json_file)
